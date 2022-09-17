462925189da3003956c6efd789d82c39d6f536d4
Bruno Coronado <brunocoronado@MacBook-Air-de-Bruno.local>
2022-09-17 16:43:46 -0500

    todos los tipos de datos basicos

--- datatype.py
+++ datatype.py
@@ -6,16 +6,31 @@ print(cadena)
 number = 30
 print(number)
 
-# Floar
+# Float
 float = 30.5
 print(float)
 
 # Boolean
-boleano = True
+boleano = True 
 print(boleano)
+
+# List
+list = [10, 20, 30, 40]
+print(list)
+
+# Tuples
+tuple = (20, 30, 40, 55)
+print(tuple)
+
+ # Dictionaries
+dic = {"Fransico": "Bruno"}
+print(dic)
 
 # Muestra por consola el tipo de dato
 print(type(cadena))
 print(type(number))
 print(type(float))
 print(type(boleano))
+print(type(list))
+print(type(tuple))
+print(type(dic))
