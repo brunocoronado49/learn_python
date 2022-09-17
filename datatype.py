99d857d1689df7887d704e9de6ce17dd696ee547
Bruno Coronado <brunocoronado@MacBook-Air-de-Bruno.local>
2022-09-17 15:53:04 -0500

    Hola mundo en python y algumos tipos de datos. Empezando a aprender

---
+++ datatype.py
@@ -0,0 +1,21 @@
+# String
+cadena = "Hola soy un string"
+print(cadena)
+
+# Int
+number = 30
+print(number)
+
+# Floar
+float = 30.5
+print(float)
+
+# Boolean
+boleano = True
+print(boleano)
+
+# Muestra por consola el tipo de dato
+print(type(cadena))
+print(type(number))
+print(type(float))
+print(type(boleano))
---
+++ helloworld.py
@@ -0,0 +1,4 @@
+# Hola este es un comentario de python
+# Este codigo imprime un mensaje por consola
+
+print("Hello world")
