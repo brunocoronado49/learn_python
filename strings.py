3330298885b00b1725b4abbc597c0f7b960cd9a1
Bruno Coronado <brunocoronado@MacBook-Air-de-Bruno.local>
2022-09-19 20:59:33 -0500

    Conociendo algunos metodos de string

--- strings.py
+++ strings.py
@@ -23,3 +23,24 @@ print(char)
 # Muesrta si empieza con la palabra indicada
 start = my_str.startswith("Bruno")
 print(start)
+
+# Separa las palabras de un string basado en los espacios
+all_wrds = "Hola esta es una variable para separar"
+words = all_wrds.split()
+print(words)
+
+# Muestra por consola el número del indice del caracter en un string
+index = my_str.index("o")
+print(index)
+
+# Muestra la longitud del string
+length = len(my_str)
+print(length)
+
+# Muestra si el string es numérico
+num = my_str.isnumeric()
+print(num)
+
+# Muestra la posición del caracter
+pos = my_str[4]
+print(pos)
